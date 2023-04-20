4451b393ae5931ba895027831a351a9b603eee49
ZhangQiYue20110207 <120012464+ZhangQiYue20110207@users.noreply.github.com>
2023-04-20 12:35:43 +0800

    Add files via upload

--- fraction.cpp
+++ fraction.cpp
@@ -3,12 +3,13 @@
 int main()
 {
     Fraction a(1/*denominator*//*type: unsigned int*/, 2/*Numerator*//*type: unsigned int*/);
+    const Fraction b(3 ,4);
     Fraction fal1(1, 3, false);/*negative fraction*/
     const Fraction fal2(3, 7, false);
-
     unsigned int d = 5;
     unsigned int n = 6;
-    Fraction frax(n, d);
-    const Fraction b(3 ,4);
+    Fraction dm(n, d);
+    Fraction fc = a;
+    const Fraction fd = b;
     return 0;
 }
